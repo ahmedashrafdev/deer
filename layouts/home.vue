de901ee142682bf7c1502dae68373ab07a82993d
ahmedashrafdev <root@localhost.localdomain>
2020-09-27 23:39:48 +0200

    loader added

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="loading-screen">
+      <img src="https://deerpublishing.b-cdn.net/loading.gif" alt="loading" />
+    </div>
     <app-nav />
     <slider />
     <!-- <transition name="router-anim" enter-active-class="animated active zoomInLeft" leave-active-class="animated active zoomOutRight"> -->
@@ -24,5 +27,35 @@ export default {
       pageTitle: '',
     }
   },
+  mounted() {
+    setTimeout(() => {
+      const loading = document.querySelector('.loading-screen')
+
+      loading.classList.add('hidden')
+      document.getElementsByTagName('body').style.overflow = 'hidden'
+    }, 10000)
+  },
 }
 </script>
+<style scoped>
+.loading-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10000;
+}
+
+.loading-screen img {
+  width: 300px;
+  height: auto;
+}
+.loading-screen.hidden {
+  display: none !important;
+}
+</style>
